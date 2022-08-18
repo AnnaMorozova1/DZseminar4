7c9e4987456c507a35c153922085ec9e59ad90f5
Anna <anyutka901990@mail.ru>
2022-08-18 14:04:55 +0300

    Zadanie 29

---
+++ Zadanie 29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая задаёт массив из N 
+//  элементов и выводит их на экран.
+
+Console.Write("Введите колличество элементов массива: ");
+int element = int.Parse(Console.ReadLine());
+int[] mass=new int [element];
+for(int i=0; i<mass.Length; i++)
+{
+    System.Console.WriteLine("Введите элемент массива: ");
+    mass[i]=int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Вывод массива: ");
+for(int i=0; i<mass.Length; i++)
+{
+    Console.WriteLine(mass[i]);
+}
+
+
+
